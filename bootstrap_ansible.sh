2645f89c6c6f8ab03770d6c3cce47aadf4dc0269
Yoriyasu Yano <yoriy@captricity.com>
2014-04-07 16:19:42 -0700

    Fix typo

--- bootstrap_ansible.sh
+++ bootstrap_ansible.sh
@@ -5,5 +5,5 @@ apt-get update
 apt-get install -y software-properties-common python-software-properties
 apt-add-repository -y ppa:rquillo/ansible
 apt-get update
-apt-get install -y ansible"
+apt-get install -y ansible
 
